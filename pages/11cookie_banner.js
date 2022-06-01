7ff33d2c718543b8325c245208d7830f096ca5d3
KenZ <zhdhui@gmail.com>
2022-06-01 10:44:10 +1000

    css color

--- pages/11cookie_banner.js
+++ pages/11cookie_banner.js
@@ -5,6 +5,7 @@ import styles from '../styles/11cookie_banner.module.css';
 export default function ArrowButton() {
   return (
     <div style={{ display: 'flex', margin: '10px' }}>
+      <div>welcome to our planet</div>
       <div className={styles.cookie_banner}>
         <div className={styles.banner_text}>welcome, please note</div>
         <div className={styles.banner_button}>Accept</div>
