da7f0d39f22eb9d29c3dee52aae60df45efdf228
KenZ <zhdhui@gmail.com>
2022-06-01 11:34:00 +1000

    conner band

---
+++ pages/12conner_band.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/12conner_band.module.css';
+
+export default function ArrowButton() {
+  return (
+    <div style={{ display: 'flex', margin: '10px' }}>
+      <div className={styles.container}>
+        <div className={styles.container__wrapper}>
+          <div className={styles.container__ribbon}>newxxxxxx</div>
+        </div>
+      </div>
+    </div>
+  );
+}
