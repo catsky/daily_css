a63a30fe675c7876ab51ef992787728de90b4749
KenZ <zhdhui@gmail.com>
2022-06-13 13:36:07 +1000

    add liner

---
+++ pages/19fadingsection.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/19fadingsection.module.css';
+
+export default function ArrowButton() {
+  return (
+    <div style={{ margin: '10px' }}>
+      <div className={styles.container}>
+        <div className={styles.container__content}>
+          wecome sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfwecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfwecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfwecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfwecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfwecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfwecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfwecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasfecome sasflas
+          asfsafdasfecome sasflas asfsafdasfecome sasflas asfsafdasfecome
+          sasflas asfsafdasfecome sasflas asfsafdasf
+        </div>
+        <div className={styles.container__fading}></div>
+      </div>
+    </div>
+  );
+}
