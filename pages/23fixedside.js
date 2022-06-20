01ab028c31f331fe04ac9f1f42e05b5d4be3b008
KenZ <zhdhui@gmail.com>
2022-06-21 09:28:55 +1000

    23 fixed side

---
+++ pages/23fixedside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/23fixedside.module.css';
+
+export default function ArrowButton() {
+  return (
+    <div>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
