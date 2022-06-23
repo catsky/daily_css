dfcf6cc109c8e50357c338fd8dcb6c142bed08c7
KenZ <zhdhui@gmail.com>
2022-06-23 11:47:06 +1000

    full background

---
+++ pages/25fullbackground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/25fullbackground.module.css';
+
+export default function ArrowButton() {
+  return (
+    <div>
+      <div className={styles.container}>welcome</div>
+    </div>
+  );
+}
