e3f33e261abc2b25c34f9afab7b4d4f65263bff5
KenZ <zhdhui@gmail.com>
2022-07-12 10:32:37 +1000

    28

---
+++ pages/28keyboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/28keyboard.module.css';
+
+export default function ArrowButton() {
+  return <div className={styles.container}>⌘ + C</div>;
+}
