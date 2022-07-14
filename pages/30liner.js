e2c0cc4eae94b929160ec71e7caa4bbdeb52a163
KenZ <zhdhui@gmail.com>
2022-07-14 11:26:01 +1000

    30 liner

---
+++ pages/30liner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/30liner.module.css';
+
+export default function ArrowButton() {
+  return (
+    <div className={styles.container}>
+      Cascading Style Sheets (CSS) is a style sheet language used for describing
+      the presentation of a document written in a markup language like HTML. CSS
+      is a cornerstone technology of the World Wide Web, alongside HTML and
+      JavaScript.
+    </div>
+  );
+}
