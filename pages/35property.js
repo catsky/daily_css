6f46d59f54aa5628e3271ba9571e37a1507bec3e
KenZ <zhdhui@gmail.com>
2022-07-21 10:12:27 +1000

    add

---
+++ pages/35property.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/35property.module.css';
+
+export default function ArrowButton() {
+  return (
+    <dl className={styles.container}>
+      <dt>Coffie</dt>
+      <dd>10% sugar and coffie</dd>
+    </dl>
+  );
+}
